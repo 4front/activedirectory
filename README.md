16b163df3e6e5c94f6073c7da5de132c9d080016
David Von Lehman <david.vonlehman@nordstrom.com>
2015-06-03 16:28:16 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 4front-activedirectory
+
+![http://4front.io](https://s3-us-west-2.amazonaws.com/4front-media/4front-logo.png)
+
+Active Directory user identity provider for [4front](http://4front.io) multi-tenant web app platform.
+
+## License
+Licensed under the [Apache License, Version 2.0](http://www.apache.org/licenses/LICENSE-2.0.html).
---
+++ index.js
@@ -0,0 +1,45 @@
+var ActiveDirectory = require('activedirectory');
+var debug = require('debug');
+
+module.exports = function(settings) {
+  if (!settings.ldapBaseDN)
+    throw new Error("Missing ldapBaseDN setting");
+
+  if (!settings.ldapUrl)
+    throw new Error("Missing ldapBaseDN setting");
+
+  var ad = new ActiveDirectory({
+    url: settings.ldapUrl,
+    baseDN: settings.ldapBaseDN
+  });
+
+  return {
+    name: 'Active Directory',
+    authenticate: function(username, password, callback) {
+      debug("authenticate user");
+
+      ad.authenticate(username, password, function(err, authenticated) {
+        if (err)
+          return callback(err);
+
+        debug("user %s authenticated", username);
+        callback(null, {username, username});
+
+        // "cn=readonly,cn=users,dc=myorg,dc=com"
+        //TODO: Would be cool to lookup the user details to
+        // get a nicer display name. For now just returning the
+        // username itself.
+        // http://stackoverflow.com/questions/17795007/node-js-ldap-auth-user
+        // ad.findUser("", function(err, user) {
+        //   if (err) return callback(err);
+        //
+        //   debugger;
+        //   console.log("user %o", user);
+        //
+        //   // Lookup the user to get their display name?
+        //   callback(null, {username: username});
+        // });
+      });
+    }
+  };
+};
