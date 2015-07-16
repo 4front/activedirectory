7e1639fd15dcc7bb587926ba621d0a9d808255dd
David Von Lehman <david.vonlehman@nordstrom.com>
2015-07-16 11:35:54 -0700

    Fixing authenticate require

--- index.js
+++ index.js
@@ -4,11 +4,14 @@ var debug = require('debug')('4front:ldap-auth');
 require('simple-errors');
 
 module.exports = function(options) {
+  if (!options.ldap)
+    throw new Error("Missing ldap object setting");
+
   // The main export is the middleware function so it can be declared in package.json router.
   var exports = require('./lib/middleware')(options);
 
   // Expose the authenticate function so it can be invoked in non-middleware scenarios.
-  exports.authenticate = ldapAuth;
+  exports.authenticate = require('./lib/ldap')(options.ldap);
 
   // Translate the username to the unique userId. In the case of LDAP just assume that the
   // userId and the username are the same.
@@ -17,7 +20,6 @@ module.exports = function(options) {
   };
 
   exports.providerName = 'ldap';
-  exports.auth = require('./lib/auth');
 
   return exports;
 };
